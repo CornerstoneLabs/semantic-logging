3f237fb193e2008109de2bccd0efa467dbc1e60f
Adam Auckland <adamauckland@gmail.com>
2017-01-23 12:05:41 +0000

    bump

--- spec/logger/SemanticLogger.spec.js
+++ spec/logger/SemanticLogger.spec.js
@@ -1,9 +1,17 @@
 describe("SemanticLogger", () => {
 	var path = require("path");
 	console.log("CWD: ", path.resolve('.'));
+	var src = path.resolve('./src');
+	console.log('SRC', src);
+	var fs = require('fs');
+	fs.readdir(src, function (err, files) {
+		files.forEach(function (f) {
+			console.log(f);
+		});
+	});
 
-	var webLogger = require("./src/webLogger.js");
-	var baseLogger = require("./src/semanticLogger.js");
+	var webLogger = require("../../src/webLogger.js");
+	var baseLogger = require("../../src/semanticLogger.js");
 	var info = baseLogger.info;
 	var debug = baseLogger.debug;
 	var warn = baseLogger.warn;
